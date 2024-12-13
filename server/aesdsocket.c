3c55910796fdc858650089552b885d48e819fd3a
eborist <eborist@Ubuntu.myguest.virtualbox.org>
2024-12-13 13:57:49 +0100

    Added working version

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -12,148 +12,184 @@
 #include <stdbool.h>
 
 #define PORT 9000
-#define FILE_PATH "/var/tmp/aesdsocketdata"
-#define BUFFER_SIZE 1024
+#define FILENAME "/var/tmp/aesdsocketdata.txt"
+#define BUFFER_SIZE 50000
 
 int sockfd = -1;
-int client_fd = -1;
-bool run_server = true;
+int running = 1;
 
 // Signal handler for SIGINT and SIGTERM
-void signal_handler(int signo) {
-    syslog(LOG_INFO, "Caught signal, exiting");
-
-    if (client_fd != -1) close(client_fd);
+void handle_signal(int signo) {
+    running = 0;
     if (sockfd != -1) close(sockfd);
+    syslog(LOG_INFO, "Server shutting down...");
 
-    remove(FILE_PATH);
+    if (remove(FILENAME) == 0) {
+        syslog(LOG_INFO, "Deleted file: %s", FILENAME);
+    } else {
+        syslog(LOG_ERR, "Failed to delete file: %s", FILENAME);
+    }
     exit(0);
 }
 
-// Daemonize the process
+void setup_signal_handling() {
+    struct sigaction sa;
+    sa.sa_handler = handle_signal;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+
+    sigaction(SIGTERM, &sa, NULL);
+    sigaction(SIGINT, &sa, NULL);
+}
+
+void handle_client(int client_fd) {
+    char buffer[BUFFER_SIZE];
+    ssize_t bytes_received;
+
+    // Receive message from client
+    bytes_received = recv(client_fd, buffer, BUFFER_SIZE, 0);
+    if (bytes_received <= 0) {
+        syslog(LOG_ERR, "Error receiving data from client");
+        close(client_fd);
+        return;
+    }
+
+    // Save message to file
+    int file_fd = open(FILENAME, O_WRONLY | O_CREAT | O_APPEND, 0644);
+    if (file_fd < 0) {
+        syslog(LOG_ERR, "Error opening file for writing");
+        close(client_fd);
+        return;
+    }
+
+    if (write(file_fd, buffer, bytes_received) < 0) {
+        syslog(LOG_ERR, "Error writing to file");
+        close(file_fd);
+        close(client_fd);
+        return;
+    }
+    close(file_fd);
+
+    // Read the content of the file and send it back to the client
+    file_fd = open(FILENAME, O_RDONLY);
+    if (file_fd < 0) {
+        syslog(LOG_ERR, "Error opening file for reading");
+        close(client_fd);
+        return;
+    }
+
+    while ((bytes_received = read(file_fd, buffer, BUFFER_SIZE)) > 0) {
+        if (send(client_fd, buffer, bytes_received, 0) < 0) {
+            syslog(LOG_ERR, "Error sending data to client");
+            close(file_fd);
+            close(client_fd);
+            return;
+        }
+    }
+
+    close(file_fd);
+    syslog(LOG_INFO, "Connection closed with client.");
+    close(client_fd);
+}
+
 void daemonize() {
     pid_t pid = fork();
     if (pid < 0) {
-        perror("fork failed");
+        perror("Fork failed");
         exit(EXIT_FAILURE);
     }
+
     if (pid > 0) {
-        exit(EXIT_SUCCESS); // Parent process exits
+        exit(EXIT_SUCCESS);
     }
 
-    // Create a new session
     if (setsid() < 0) {
         perror("setsid failed");
         exit(EXIT_FAILURE);
     }
 
-    // Change the working directory
+    pid = fork();
+    if (pid < 0) {
+        perror("Second fork failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pid > 0) {
+        exit(EXIT_SUCCESS);
+    }
+
     if (chdir("/") < 0) {
         perror("chdir failed");
         exit(EXIT_FAILURE);
     }
 
-    // Redirect standard file descriptors
     close(STDIN_FILENO);
     close(STDOUT_FILENO);
     close(STDERR_FILENO);
     open("/dev/null", O_RDONLY);
-    open("/dev/null", O_WRONLY);
+    open("/dev/null", O_RDWR);
     open("/dev/null", O_RDWR);
 }
 
 int main(int argc, char *argv[]) {
     struct sockaddr_in server_addr, client_addr;
     socklen_t client_len = sizeof(client_addr);
-    char buffer[BUFFER_SIZE];
-    char client_ip[INET_ADDRSTRLEN];
 
-    // Open syslog
-    openlog("aesdsocket", LOG_PID | LOG_CONS, LOG_USER);
+    openlog("aesdserver", LOG_PID | LOG_CONS, LOG_USER);
+    setup_signal_handling();
 
-    // Check for daemon mode
-    if (argc > 1 && strcmp(argv[1], "-d") == 0) {
-        daemonize();
-    }
-
-    // Set up signal handling
-    signal(SIGINT, signal_handler);
-    signal(SIGTERM, signal_handler);
-
-    // Create socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) {
         syslog(LOG_ERR, "Socket creation failed: %s", strerror(errno));
         exit(EXIT_FAILURE);
     }
 
-    // Set up server address structure
+    int opt = 1;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) < 0) {
+        syslog(LOG_ERR, "setsockopt failed: %s", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     server_addr.sin_port = htons(PORT);
 
-    // Bind socket
     if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         syslog(LOG_ERR, "Socket bind failed: %s", strerror(errno));
         close(sockfd);
         exit(EXIT_FAILURE);
     }
 
-    // Listen for connections
+    if (argc > 1 && strcmp(argv[1], "-d") == 0) {
+        syslog(LOG_INFO, "Running as daemon");
+        daemonize();
+    }
+
     if (listen(sockfd, 10) < 0) {
         syslog(LOG_ERR, "Socket listen failed: %s", strerror(errno));
         close(sockfd);
         exit(EXIT_FAILURE);
     }
 
-    while (run_server) {
-        // Accept client connection
-        client_fd = accept(sockfd, (struct sockaddr *)&client_addr, &client_len);
+    syslog(LOG_INFO, "Server listening on port %d", PORT);
+
+    while (running) {
+        int client_fd = accept(sockfd, (struct sockaddr *)&client_addr, &client_len);
         if (client_fd < 0) {
             syslog(LOG_ERR, "Accept failed: %s", strerror(errno));
             continue;
         }
 
+        char client_ip[INET_ADDRSTRLEN];
         inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, INET_ADDRSTRLEN);
         syslog(LOG_INFO, "Accepted connection from %s", client_ip);
 
-        // Open file for appending and reading
-        FILE *file = fopen(FILE_PATH, "a+");
-        if (!file) {
-            syslog(LOG_ERR, "Failed to open file: %s", strerror(errno));
-            close(client_fd);
-            continue;
-        }
-
-        // Receive data from client
-        ssize_t bytes_received;
-        while ((bytes_received = recv(client_fd, buffer, BUFFER_SIZE, 0)) > 0) {
-            fwrite(buffer, 1, bytes_received, file);
-            fflush(file);
-
-            if (strchr(buffer, '\n')) break;
-        }
-
-        if (bytes_received < 0) {
-            syslog(LOG_ERR, "Receive failed: %s", strerror(errno));
-        }
-
-        // Send back file contents to client
-        rewind(file);
-        while (fgets(buffer, sizeof(buffer), file)) {
-            send(client_fd, buffer, strlen(buffer), 0);
-        }
-
-        fclose(file);
-        syslog(LOG_INFO, "Closed connection from %s", client_ip);
-        close(client_fd);
-        client_fd = -1;
+        handle_client(client_fd);
     }
 
-    // Cleanup and exit
-    close(sockfd);
     closelog();
-    return 0;
+    close(sockfd);
+    return EXIT_SUCCESS;
 }
 
